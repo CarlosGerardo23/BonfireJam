7280b8c5fed57acfe469c07cf5bee9badd64cc46
Carlos23 <carlos.enriquez.valerio@gmail.com>
2020-08-28 21:09:42 -0500

    Music initial

---
+++ Bonfire/Assets/Scripts/InstrumentsScripts/InstrumentBehaviout.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeInstrument { TAP, HOLD, MIXED }
+public class InstrumentBehaviout : MonoBehaviour
+{
+   [SerializeField] TypeInstrument instrument;
+   List<GameObject> poitnsOfInterest;
+   [SerializeField] Sprite bigCircle;
+   [SerializeField] Sprite smallCircle;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   public void ActionEventOfInterest()
+   {
+      switch (instrument)
+      {
+         case TypeInstrument.TAP:
+            break;
+         case TypeInstrument.HOLD:
+            break;
+         case TypeInstrument.MIXED:
+            break;
+         default:
+            break;
+      }
+   }
+
+   void TapEvent()
+   {
+
+   }
+   void HoldEvent()
+   {
+
+   }
+   void MixedEvent()
+   {
+
+   }
+}
--- Bonfire/Assets/Scripts/UIScripts/StartGameMenu.cs
+++ Bonfire/Assets/Scripts/UIScripts/StartGameMenu.cs
@@ -5,15 +5,19 @@ using UnityEngine.InputSystem;
 
 public class StartGameMenu : UIElement
 {
+  public int index;
+  public bool isPressed=false;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        player.UIGameplay.TestStart.performed += test => StartPressed();
-    }
-    void StartPressed()
-    {
-        Debug.Log("Presione start");
-    }
+  // Start is called before the first frame update
+  public override void Awake()
+  {
+    base.Awake();
+    player.UIGameplay.TestStart.performed += test => StartPressed();
+  }
+  void StartPressed()
+  {
+    
+    Debug.Log("Presione start: " + index.ToString());
+  }
 
 }
