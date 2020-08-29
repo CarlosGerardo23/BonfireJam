48c5613aaac07dbd90433c979d9ddbb5b1244d4a
Diego Clorio <diego_clorio@hptmail.com>
2020-08-29 14:38:18 -0500

    Avance

---
+++ Bonfire/Assets/GrowthScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowthScript : MonoBehaviour
+{
+    private bool grow;
+    public bool animPlaying;
+
+    public bool Grow
+    {
+        get { return grow; }
+        set { grow = value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grow)
+        {
+            grow = false;
+            animPlaying = true;
+            GetComponent<Animator>().SetTrigger("grow");
+            StartCoroutine(isAnimOver());
+        }
+    }
+
+    IEnumerator isAnimOver()
+    {
+        yield return new WaitForSeconds(.5f);
+        animPlaying = false;
+    }
+}
