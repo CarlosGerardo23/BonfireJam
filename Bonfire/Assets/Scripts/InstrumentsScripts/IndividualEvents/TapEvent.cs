9446a20eb809e493d3891fa9650e4d0eed216b57
Carlos23 <carlos.enriquez.valerio@gmail.com>
2020-08-30 07:11:28 -0500

    Cambio de color tap event

--- Bonfire/Assets/Scripts/InstrumentsScripts/IndividualEvents/TapEvent.cs
+++ Bonfire/Assets/Scripts/InstrumentsScripts/IndividualEvents/TapEvent.cs
@@ -6,11 +6,14 @@ public class TapEvent : InstrumentEvent
 {
    [SerializeField] Vector3 minimumScale;
    [SerializeField] Vector3 maxScale;
-
+   [SerializeField] Color bigguerCircleColor;
+   [SerializeField] GameObject bigguerCircleReference;
+   Color defaultBigguerCircleColor;
 
    protected override void ActivateEvent()
    {
       animator.SetTrigger("activate");
+      defaultBigguerCircleColor = bigguerCircleReference.GetComponent<SpriteRenderer>().color;
 
       eventStarted = true;
    }
@@ -24,15 +27,17 @@ public class TapEvent : InstrumentEvent
       if (compare.localScale.magnitude < minimumScale.magnitude
           && compare.localScale.magnitude > maxScale.magnitude)
       {
+         bigguerCircleReference.GetComponent<SpriteRenderer>().color = bigguerCircleColor;
          canDoAction = true;
       }
-      if(compare.localScale.magnitude <= maxScale.magnitude)
+      if (compare.localScale.magnitude <= maxScale.magnitude)
       {
+         bigguerCircleReference.GetComponent<SpriteRenderer>().color = defaultBigguerCircleColor;
          animationOver = true;
          canDoAction = false;
          compare.localScale = new Vector3(1, 1, 1);
          CheckLifeOfEvent();
-        
+
       }
 
    }
