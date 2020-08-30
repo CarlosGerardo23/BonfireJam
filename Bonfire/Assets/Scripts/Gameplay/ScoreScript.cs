0666336ea74922ef07fb121d0a59ea8e07149d3d
Diego Clorio <diego_clorio@hptmail.com>
2020-08-30 11:04:57 -0500

    hide coursor

--- Bonfire/Assets/Scripts/Gameplay/ScoreScript.cs
+++ Bonfire/Assets/Scripts/Gameplay/ScoreScript.cs
@@ -9,6 +9,8 @@ public class ScoreScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         rotated = false;
         score = 5;
     }
